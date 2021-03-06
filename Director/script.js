3862929226df0e6fe60eaab179fab6d8973106b1
KETAN SHARMA <ketansharma957@gmail.com>
2021-03-06 21:12:30 +0530

    Addition of Director Profile

---
+++ Director/script.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "7 Aug 2021";
+
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
